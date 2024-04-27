49e2c719249a30f1ad8c8494f48dd910a1ed61c3
obich <g.shchet@gmail.com>
2024-04-27 16:15:56 +0300

    didn't changed json creating

--- excel_diary.py
+++ excel_diary.py
@@ -61,41 +61,89 @@ answer = {
         'Лушникова Т.Б.': ['Биология']
     }
 }
+
+
+
+def add_class_to_dict(class_name):
+    answer["classes"][class_name] = {
+        "Понедельник": [{"subject": "", "teacher": "", "classroom": ""} for n in range(1, 9)],
+        "Вторник": [{"subject": "", "teacher": "", "classroom": ""} for n in range(1, 8)],
+        "Среда": [{"subject": "", "teacher": "", "classroom": ""} for n in range(1, 8)],
+        "Четверг": [{"subject": "", "teacher": "", "classroom": ""} for n in range(1, 8)],
+        "Пятница": [{"subject": "", "teacher": "", "classroom": ""} for n in range(1, 8)]
+    }
+
+def insert_lesson_data(current_school_class, current_day, lesson_data):
+    print(lesson_data)
+    days_of_the_week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница"]
+    subject = lesson_data[0]
+    classrom = lesson_data[1]
+    teacher = lesson_data[2:]
+    lesson_number = counter // 8
+    if lesson_number == 1:
+        current_day += 1
+    lesson = answer["classes"][current_school_class][days_of_the_week[current_day]][lesson_number - 1]       #мб изменить и ускорить алгоритм
+    lesson['lesson'] = subject
+    lesson['classrom'] = classrom
+    lesson['teacher'] = teacher
+
 # iterate through excel and display data
-for row in ws.iter_rows(min_row=3, max_col=37, min_col=2, max_row=51, values_only=True):
-    teacher = row[0]
-    day_of_the_week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница"]
-    day = -1
-    for index, school_class in enumerate(row[1:]):
-        if index % 7 == 0:
-            day += 1
-        if school_class:
-            if len(school_class) <= 3:
-                if school_class not in answer["classes"]:
-                    answer["classes"][school_class] = {
-                        "Понедельник": {n: "" for n in range(1, 9)},
-                        "Вторник": {n: "" for n in range(1, 8)},
-                        "Среда": {n: "" for n in range(1, 8)},
-                        "Четверг": {n: "" for n in range(1, 8)},
-                        "Пятница": {n: "" for n in range(1, 8)}
-                    }
-                else:
-                    if day == 0 and index % 7 == 1:
-                        answer['classes'][school_class][day_of_the_week[day]][index % 7] = 'Разговоры о важном'
-                    answer['classes'][school_class][day_of_the_week[day]][index % 7 + 1] = teachers_dict[teacher][0]
-            else:
-                divided_class = school_class.split(', ')
-                for i in divided_class:
-                    if i not in answer["classes"]:
-                        answer["classes"][i] = {
-                            "Понедельник": {n: "" for n in range(1, 9)},
-                            "Вторник": {n: "" for n in range(1, 8)},
-                            "Среда": {n: "" for n in range(1, 8)},
-                            "Четверг": {n: "" for n in range(1, 8)},
-                            "Пятница": {n: "" for n in range(1, 8)}
-                        }
-                    else:
-                        if day == 0 and index % 7 == 1:
-                            answer['classes'][i][day_of_the_week[day]][index % 7] = 'Разговоры о важном'
-                        answer['classes'][i][day_of_the_week[day]][index % 7 + 1] = teachers_dict[teacher][0]
+filled_columns = [i.coordinate for i in list(ws.rows)[0]][2:]
+print(filled_columns)
+wrkbk2 = openpyxl.load_workbook("diary_files/Diary2.xlsx")
+counter = 0
+current_day = -1
+current_school_class = ''
+ws2 = wrkbk2.active
+for column in filled_columns:
+    for cell in ws2[column[:-1]]:
+
+        if counter == 0:
+            current_school_class = cell.value
+            add_class_to_dict(current_school_class)
+            counter += 1
+        else:
+            if cell.value is not None and cell.value != 'УПК':
+                lesson_data = cell.value.split()
+                lesson_number = counter // 8
+                if lesson_number == 1:
+                    current_day += 1
+                insert_lesson_data(current_school_class, current_day, lesson_data)
+
+# for row in ws.iter_rows(min_row=3, max_col=37, min_col=2, max_row=51, values_only=True):
+#     teacher = row[0]
+#     day_of_the_week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница"]
+#     day = -1
+#     for index, current_school_class in enumerate(row[1:]):
+#         if index % 7 == 0:
+#             day += 1
+#         if current_school_class:
+#             if len(current_school_class) <= 3:
+#                 if current_school_class not in answer["classes"]:
+#                     answer["classes"][current_school_class] = {
+#                         "Понедельник": {n: "" for n in range(1, 9)},
+#                         "Вторник": {n: "" for n in range(1, 8)},
+#                         "Среда": {n: "" for n in range(1, 8)},
+#                         "Четверг": {n: "" for n in range(1, 8)},
+#                         "Пятница": {n: "" for n in range(1, 8)}
+#                     }
+#                 else:
+#                     if day == 0 and index % 7 == 1:
+#                         answer['classes'][current_school_class][day_of_the_week[day]][index % 7] = 'Разговоры о важном'
+#                     answer['classes'][current_school_class][day_of_the_week[day]][index % 7 + 1] = teachers_dict[teacher][0]
+#             else:
+#                 divided_class = current_school_class.split(', ')
+#                 for i in divided_class:
+#                     if i not in answer["classes"]:
+#                         answer["classes"][i] = {
+#                             "Понедельник": {n: "" for n in range(1, 9)},
+#                             "Вторник": {n: "" for n in range(1, 8)},
+#                             "Среда": {n: "" for n in range(1, 8)},
+#                             "Четверг": {n: "" for n in range(1, 8)},
+#                             "Пятница": {n: "" for n in range(1, 8)}
+#                         }
+#                     else:
+#                         if day == 0 and index % 7 == 1:
+#                             answer['classes'][i][day_of_the_week[day]][index % 7] = 'Разговоры о важном'
+#                         answer['classes'][i][day_of_the_week[day]][index % 7 + 1] = teachers_dict[teacher][0]
 print(answer)
